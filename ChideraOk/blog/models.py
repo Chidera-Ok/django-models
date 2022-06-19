f3df9f6f17dc4ed21380e570177cdaa14f17a804
Chidera-Ok <okekechidera98@gmail.com>
2022-06-19 06:06:37 -0700

    second commit

---
+++ ChideraOk/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(user, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+
+
